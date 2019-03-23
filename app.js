b85e722e5537ec2c606ef1badccfcc53fee86a5f
Maggie <mchen1415@gmail.com>
2019-03-22 23:34:33 -0400

    Add authorization to users

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const passport = require('passport');
+require('./config/passport')(passport);
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -20,5 +23,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+app.use(passport.initialize());
 
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
@@ -15,7 +16,7 @@ router.post('/register', (req, res) => {
           handle: req.body.handle,
           email: req.body.email,
           password: req.body.password
-        })
+        });
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -62,6 +63,13 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+});
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
