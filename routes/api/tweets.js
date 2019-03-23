083f976a479177b92f757ec78f21a5039a568f14
Maggie <mchen1415@gmail.com>
2019-03-23 17:55:45 -0400

    Update Tweet model

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-
 const Tweet = require('../../models/Tweet');
 const validateTweetInput = require('../../validation/tweets');
 
