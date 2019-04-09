714e93e953db954f3fb3ac8275d917eb091c4938
Maggie Chen <mchen1415@gmail.com>
2019-04-09 14:34:39 -0400

    Add limit and skip to comic route

--- routes/api/comics.js
+++ routes/api/comics.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const Comic = require('../../models/Comic');
 
 router.get('/', (req, res) => {
+    const { skip = 0, limit = 10} = req.query;
+
     if (req.query.searchedQuery) {
         const regex = new RegExp(escapeRegex(req.query.searchedQuery), 'gi');
-        Comic.find({ "dialog": regex })
+        Comic.find({ "dialog": regex }, {
+            skip: Number(skip),
+            limit: Number(limit)
+        })
             .sort({ date: 1 })
             .then(comics => res.json(comics))
             .catch(err => res.status(404).json({ nocomicsfound: 'No comics found' }));   
