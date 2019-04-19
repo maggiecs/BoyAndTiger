0ea6ae75244f7829bcf950d09842983087dba9e8
Gary Kang <garyzkang@gmail.com>
2019-04-19 14:30:04 -0400

    Configure for Heroku deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const users = require("./routes/api/users");
 const comics = require("./routes/api/comics");
 const passport = require('passport');
 require('./config/passport')(passport);
+const path = require("path");
 
 
 mongoose
@@ -14,6 +15,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,5 +31,3 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use("/api/users", users);
 app.use("/api/comics", comics);
 app.use(passport.initialize());
-
-
